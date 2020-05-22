85b2b55419fe771b664cf6e3bc703d77a1c3f5b3
jaques <jaques.oliveira@gmail.com>
2020-05-21 22:43:30 -0300

    criado o arquivo mongojs

---
+++ db/mongo.js
@@ -0,0 +1,22 @@
+var mongojs = require('mongojs'),
+    config = require('mongojs'),
+    debug = require('debug')('livro_nodejs');
+    'use strict';
+
+    function _conection(){
+        var username = config.get('mongo.username'),
+        password = config.get('mongo.password'),
+        server = config.get('mongo.server'),
+        port = config.get('mongo.port'),
+        database = config.get('mongo.database'),
+        auth = username ? username + ':' + password + '@' : '';
+
+        return 'mongodb://' + auth + server + ':' + port + '/' + database;
+    }
+
+    var db = mongojs(_connection());
+    db.on('error', function(err){
+        debug(err);
+    });
+
+    module.exports = db;
