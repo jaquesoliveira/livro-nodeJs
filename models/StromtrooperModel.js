c5750fa3aea9e139ee548b6086adfe5d06c99f51
jaques <jaques.oliveira@gmail.com>
2020-05-21 22:19:16 -0300

    create model

---
+++ models/StromtrooperModel.js
@@ -0,0 +1,9 @@
+function StormtrooperModel(){}
+
+StormtrooperModel.prototype.find = function(query, callback){};
+
+StormtrooperModel.prototype.create = function(data, callback){};
+
+StormtrooperModel.prototype.update = function(_id, data, callback){};
+
+StormtrooperModel.prototype.remove = function(_id, callback){};
